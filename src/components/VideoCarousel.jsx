fb2a3f535cb0c3c6dedc8091a998806731386241
Eslam Abdallah <eslamabdallah301@gmail.com>
2024-11-04 15:36:14 +0200

    add silders

---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,30 @@
+
+import { highlightsSlides } from "../constants";
+const VideoCarousel = () => {
+  return (
+    <>
+      <div className="flex items-center">
+        {highlightsSlides.map((slide, index) => (
+          <div key={index} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video id="video" muted playsInline={true}>
+                  <source src={slide.video} type="video/mp4" />
+                </video>
+              </div>
+              <div className="absolute top-12 left-[5%] z-10">
+                {slide.textLists.map((text, index) => (
+                  <p key={index} className="md:text-2xl text-xl font-medium">
+                    {text}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default VideoCarousel
